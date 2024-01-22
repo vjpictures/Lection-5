6151cd26304e6ff1bd97f14c64c5c9dd7683b39d
vjpictures <vjpictures@mail.ru>
2024-01-22 20:01:51 +0100

    changes in task

--- src/task/arrays/TaskArrays.java
+++ src/task/arrays/TaskArrays.java
@@ -1,40 +1,47 @@
 package task.arrays;
 
 public class TaskArrays {
-    public static void main(String[] args) {
-        int[][] result = new int[2][5];
-        int[][] array = {
-                {0,1},
-                {2,3},
-                {4,5},
-                {6,7},
-                {8,9},
-        };
-        for (int[] ints : array) {
-            for (int anInt : ints) {
-                System.out.print(anInt + " ");
-            }
-            System.out.println();
-        }
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[i].length; j++) {
-                result[i][j] = array[j][i];
+    public static int[][] transpose(int[][] array) {
+        int rows = array.length;
+        int columns = array[0].length;
+
+        int[][] transposeArray = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                transposeArray[j][i] = array[i][j];
             }
         }
-        System.out.println("------");
-        for (int[] ints : result) {
-            for (int anInt : ints) {
-                System.out.print(anInt + " ");
+        return transposeArray;
+    }
+
+    public static void printArray(int[][] array) {
+        for (int[] row : array) {
+            for (int value : row) {
+                System.out.print(value + " ");
             }
             System.out.println();
         }
     }
 
-    public static class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        int[][] array = {
+                {0, 1},
+                {2, 3},
+                {4, 5},
+                {6, 7},
+                {8, 9},
+        };
 
-        }
+        int[][] transposeArray = transpose(array);
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        System.out.println("\nTranspose Array:");
+        printArray(transposeArray);
     }
 }
+
 
 
